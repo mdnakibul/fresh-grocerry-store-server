d9415c55fd09a1792c850978ec3f8137867a1d4e
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-04-07 19:01:17 -0600

    port updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mymds.mongodb.net/emaJohnStore?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mymds.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("freshGroceryStore").collection("products");
@@ -100,4 +100,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(process.env.PORT || port)
